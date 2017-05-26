d39a9137fd71cec2d6279bd98e3040995e2ecdcd
nguyenlyminhman <nguyenlyminhman@gmail.com>
2017-05-26 18:06:14 +0700

    final stage

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const urlencdeParser = bodyParser.urlencoded({extended:false});
-const nodeMail = require('nodemailer');
+const Parser = bodyParser.urlencoded({ extended: false });
+const nodemailer = require('nodemailer');
 const hbs = require('nodemailer-express-handlebars');
 
 
 app.set('view engine', 'ejs');
 app.set('views', 'views')
-app.get('/', (req,res)=> res.render('home'));
+app.get('/', (req, res) => res.render('home'));
 
 app.listen('3000', console.log('home'));
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: 'mannlmgt60850@fpt.edu.vn',
+        pass: 'Yokk@ichi8'
+    }
+});
+
+transporter.use('compile', hbs({
+    viewPath: 'template',
+    extName: '.ejs'
+}));
+
+
+app.post('/sendmail', Parser, (req, res) => {
+    const { mailto, title, content } = req.body;
+    transporter.sendMail({
+        from: 'mannlmgt60850@fpt.edu.vn',
+        to: mailto,
+        subject: title,
+        template: 'mail',
+        context: {
+            content
+        }
+    }, (err, res) => {
+        if (err) res.send('fail');
+        res.send('success');
+    });
+});
